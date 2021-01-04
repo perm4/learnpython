bc4c0d073f04fa4c77e00a08c0274b98f9a3f784
jaydeen <jklee050@gmail.com>
2021-01-04 16:12:17 +0900

    자료형-숫자

---
+++ number.py
@@ -0,0 +1,39 @@
+# number.py
+# 숫자형
+# 정수형
+a = 123
+print(a)
+a = -178
+print(a)
+a = 0
+print(a)
+
+# 실수형
+a = 1.2
+print(a)
+a = -3.45
+print(a)
+a = 4.24E10 # 4.24 * 10의 10제곱
+print(a)
+a = 4.24e-10 # 4.24 * 10의 -10제곱
+print(a)
+
+# 8진수와 16진수
+a = 0o177
+print(a)
+a = 0x8ff
+print(a)
+a = 0xABC
+print(a)
+
+# 사칙연산 +,-,*,/
+print(1+3)
+print(1-3)
+print(1*3)
+print(1/3)
+# 제곱 a**b
+print(2**3)
+# 나눗셈 후 나머지를 반환 %
+print(2%3)
+# 나눗셈 후 몫을 반환 //
+print(2//3)
